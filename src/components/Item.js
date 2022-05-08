dc8f28a32787c4ee8ceffa40bf5b0eac35d8f7e0
eckertr <rachel.eckert13@gmail.com>
2022-05-08 18:22:53 -0400

    commit tree dc044fcb261ebec4e6d3f22442e268b01fccbab7

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [bClick, setClick] = useState(false)
+  const toggleClass = () => {
+setClick(!bClick)
+  }
   return (
-    <li className="">
+    <li className={bClick ? "in-cart" : null}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick = {toggleClass} className="add">Add to Cart</button>
     </li>
   );
 }
